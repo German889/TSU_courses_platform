f98a0393f6762c7d0646834f8c4a85c3d89d428d
Givermaen <x41572@gmail.com>
2023-01-11 04:58:48 +0400

    added main page

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var rootPath = require('app-root-path');
+const normalize = require('normalize-path');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  //res.sendFile(rootPath +'/app_server/views/static/index-pattern.jade');
+  //normalize(rootPath);
+  res.render('static/index-pattern');
+  console.log('paaaath='+rootPath);
 });
 
 module.exports = router;
