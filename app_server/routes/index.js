f7427a5087290dd18e276880cc46efe5cf29db93
Givermaen <x41572@gmail.com>
2023-01-12 21:45:03 +0400

    added java-dev page

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,11 +4,14 @@ var rootPath = require('app-root-path');
 const normalize = require('normalize-path');
 
 router.get('/', function(req, res, next) {
-  
-  //res.sendFile(rootPath +'/app_server/views/static/index-pattern.jade');
-  //normalize(rootPath);
   res.render('static/index-pattern');
-  console.log('paaaath='+rootPath);
+  console.log('paaaath='+rootPath); // служебная информация, можно убрать
+});
+router.get('/courses', function(req, res, next){
+  res.render('static/course_market_pattern');
+});
+router.get('/java-dev', function(req, res, next){
+  res.render('static/course_pg_pattern');
 });
 
 module.exports = router;
